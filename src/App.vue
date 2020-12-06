f2866800c8cc966de762512a6105afdc367e522f
caobandoc <obandocarlosandres@gmail.com>
2020-12-05 19:25:45 -0500

    Arreglo al app y html

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <section-api></section-api>
+
+    <div id="news" class="container-fluid alert alert-dark">
+      <section-api></section-api>
+    </div>
+
+    <div id="team" class="alert alert-light">
+      <!-- TEAM -->
+    </div>
+    
   </div>
 
 </template>
